41df49b9c3f94063d076f7f1b7190a2bdca0dbef
Rikard Hevosmaa <110062271+rhmaa@users.noreply.github.com>
2022-12-23 14:12:54 +0100

    Replace xterm with urxvt
    I cannot for the life of me get proper FreeType font rendering to work
    in Xterm, which is a shame since I really like that terminal. I can
    use whatever fonts I want in urxvt though, so it will have to be a
    compromise.

--- Makefile
+++ Makefile
@@ -3,7 +3,7 @@ sync:
 	cp -f ~/.bash_profile ./dotfiles/bash_profile
 	cp -rf ~/.config/emacs ./dotfiles/
 	cp -rf ~/.config/git/config ./dotfiles/gitconfig
-	cp -f ~/.config/Xresources ./dotfiles/Xresources
+	cp -f ~/.config/xorg/urxvt.conf ./dotfiles/urxvt.conf
 	cp -f ~/.config/inputrc ./dotfiles/inputrc
 
 copy:
@@ -11,8 +11,8 @@ copy:
 	cp -f ./dotfiles/bash_profile ~/.bash_profile
 	cp -rf ./dotfiles/emacs ~/.config/
 	mkdir ~/.config/git && cp -rf ./dotfiles/gitconfig ~/.config/git/config
-	cp -f ./dotfiles/Xresources ~/.config/Xresources
-	xrdb -merge ~/.config/Xresources
+	mkdir ~/.config/xorg && cp -f ./dotfiles/urxvt.conf ~/.config/xorg/urxvt.conf
+	xrdb -merge ~/.config/urxvt.conf
 	cp -f ./dotfiles/inputrc ~/.config/inputrc
 	. ~/.bashrc
 
@@ -21,7 +21,7 @@ clean:
 	rm -f ~/.bash_profile
 	rm -rf ~/.config/emacs
 	rm -rf ~/.config/git
-	rm -f ~/.config/Xresources
+	rm -f ~/.config/xorg/urxvt.conf
 	rm -f ~/.config/inputrc
 
 .PHONY: sync copy clean
--- fedora-setup.sh
+++ fedora-setup.sh
@@ -34,7 +34,7 @@ PACKAGE_LIST=(
     steam
     transmission
     vlc
-    xterm
+    rxvt-unicode
 )
 
 FLATPAK_LIST=(
@@ -106,7 +106,7 @@ gsettings set org.gnome.settings-daemon.plugins.media-keys home "['<Super>e']"
 gsettings set org.gnome.settings-daemon.plugins.media-keys custom-keybindings "['/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/']"
 
 gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/ binding "<Super>t"
-gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/ command "xterm"
+gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/ command "rxvt-unicode"
 gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/ name "Launch terminal"
 
 
